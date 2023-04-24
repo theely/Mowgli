e9ef639ee0e926b93afdf169acda3e1fa218c732
Elia Palme <2025999+theely@users.noreply.github.com>
2023-04-24 18:27:08 +0200

    marked conflicts and todos

--- stm32/ros_usbnode/include/board.h
+++ stm32/ros_usbnode/include/board.h
@@ -33,7 +33,7 @@ E15 GPIO Drive Motors - HC366 OE Pins (LOW to enable) GPIO_MODE_OUTPUT_PP GPIO_N
 E8  TIM1_CH1N Charge Control Pins - HighSide/LowSide MosFET  
 E9  TIM1_CH1 Charge Control Pins - HighSide/LowSide MosFET 
 C0  GPIO Stop button - yellow (HIGH when pressed) GPIO_MODE_INPUT GPIO_PULLDOWN
-TODO: CONFLICT UART6 C8  GPIO Stop button - white (HIGH when pressed)  GPIO_MODE_INPUT GPIO_PULLDOWN
+TODO: CONFLICT UART6 - C8  GPIO Stop button - white (HIGH when pressed)  GPIO_MODE_INPUT GPIO_PULLDOWN
 A8  GPIO Mechanical tilt - (HIGH when set) GPIO_MODE_INPUT GPIO_PULLDOWN
 B3  SPI3 Flash CLK
 B4  SPI3 Flash MISO
@@ -41,7 +41,7 @@ B5  SPI3 Flash MISI
 A15 GPIO  Flash nCS  GPIO_MODE_OUTPUT_PP GPIO_PULLUP GPIO_SPEED_HIGH 
 D0  GPIO Wheel lift blue GPIO_MODE_INPUT GPIO_PULLDOWN
 D1  GPIO Wheel lift red  GPIO_MODE_INPUT GPIO_PULLDOWN
-TODO: CONFLICT UART6 C7  GPIO Play button - (LOW when pressed) GPIO_MODE_INPUT GPIO_PULLUP
+TODO: CONFLICT UART6 - C7  GPIO Play button - (LOW when pressed) GPIO_MODE_INPUT GPIO_PULLUP
 B13 GPIO Home button - (LOW when pressed) GPIO_MODE_INPUT GPIO_PULLUP
 E2  GPIO Rain Sensor - (LOW when active)  GPIO_MODE_INPUT GPIO_PULLUP
 D2  GPIO STOP HALL Sensor - (HIGH when set) TODO: find config
@@ -52,8 +52,10 @@ TODO: NOT FOUND C11 UART4 RX (DMA) - Master UART Red Port J18 (DMA)
 TODO: NOT FOUND C10 UART4 TX (DMA) - Master UART Red Port J18
 D6  UART2 RX (DMA) - Driver motros 
 D5  UART2 TX (DMA) - Driver motros 
-TODO: Check new port C7 - B11 UART6 RX (DMA) - Blade motor
-TODO: Check new port C6 - B10 UART6 TX (DMA) - Blade motor
+TODO: Check new port C7 or A12  - B11 UART6 RX (DMA) - Blade motoror 
+TODO: Check new port C6 or A11 - B10 UART6 TX (DMA) - Blade motor
+A12 USB_DP
+A11 USB_DM
 A10 UART1 RX (DMA) - Panle UART Port J16
 A9  UART1 TX (DMA) - Panle UART Port J16
 [not used] B3  GPIO SOFT I2C
@@ -89,6 +91,8 @@ A6  ADC1 (DMA) - Perimeter
 #define PANEL_TYPE_YARDFORCE_500_CLASSIC 1
 #define PANEL_TYPE_YARDFORCE_LUV1000RI 2
 #define PANEL_TYPE_YARDFORCE_900_ECO 3
+
+#define BOARD_YARDFORCE500 true //TODO: find proper way to define this
 
 #if defined(BOARD_YARDFORCE500)
 #define PANEL_TYPE PANEL_TYPE_YARDFORCE_500_CLASSIC
